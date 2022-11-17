cd4e2de1a38820490028d47dbf4b2e2278423115
quyetnguyen134 <69424850+quyettm134@users.noreply.github.com>
2022-11-18 00:00:58 +0700

    first init

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+//mongodb
+
+const CONNECTION_URL = 'mongodb+srv://quyettm134:13042001Q@cluster0.hnysbgv.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server listening on ${PORT}`)))
+    .catch((error) => console.log(error.message))
