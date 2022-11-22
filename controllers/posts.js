27948a6f504c25b1bf6054d7f2c2c06b396cdda6
quyetnguyen134 <69424850+quyettm134@users.noreply.github.com>
2022-11-23 00:35:13 +0700

    update

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,7 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
-export const getPosts = async (req, res, next) => {
+export const getPosts = async (req, res) => {
     try {
         const postMessage = await PostMessage.find();
 
@@ -10,7 +11,7 @@ export const getPosts = async (req, res, next) => {
     }
 }
 
-export const createPost = async (req, res, next) => {
+export const createPost = async (req, res) => {
     const post = req.body;
 
     const newPost = new PostMessage(post);
@@ -22,4 +23,16 @@ export const createPost = async (req, res, next) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+
+    res.json(updatedPost);
 }
