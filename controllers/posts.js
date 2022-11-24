2814f97857e11d9b64a0a639d2775566e22b42eb
quyetnguyen134 <69424850+quyettm134@users.noreply.github.com>
2022-11-24 21:36:10 +0700

    update

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,38 +1,71 @@
-import mongoose from 'mongoose';
-import PostMessage from '../models/postMessage.js';
+import mongoose from "mongoose";
+import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
-    try {
-        const postMessage = await PostMessage.find();
+  try {
+    const postMessage = await PostMessage.find();
 
-        res.status(200).json(postMessage);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+    res.status(200).json(postMessage);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
+};
 
 export const createPost = async (req, res) => {
-    const post = req.body;
+  const post = req.body;
 
-    const newPost = new PostMessage(post);
+  const newPost = new PostMessage(post);
 
-    try {
-        await newPost.save();
+  try {
+    await newPost.save();
 
-        res.status(200).json(newPost);
-    } catch (error) {
-        res.status(409).json({ message: error.message });
-    }
-}
+    res.status(200).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error });
+  }
+};
 
 export const updatePost = async (req, res) => {
-    const { id: _id } = req.params;
+  const { id: _id } = req.params;
 
-    const post = req.body;
+  const post = req.body;
 
-    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID');
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that ID");
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    { new: true }
+  );
 
-    res.json(updatedPost);
-}
+  res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that ID");
+
+  await PostMessage.findByIdAndDelete(_id);
+
+  res.json({ message: "Post deleted successfully" });
+};
+
+export const likePost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that ID");
+
+  const post = await PostMessage.findById(_id);
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { likeCount: post.likeCount + 1 },
+    { new: true }
+  );
+
+  res.json(updatedPost);
+};
--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
+
 //mongodb
 
-const CONNECTION_URL = 'mongodb+srv://quyettm134:13042001Q@cluster0.hnysbgv.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`Server listening on ${PORT}`)))
-    .catch((error) => console.log(error.message))
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
